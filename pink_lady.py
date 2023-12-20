47805784a14c76a214393bd8c98cde7c1b6a6e09
ACornuIGN <antoine.cornu@ign.fr>
2023-12-20 10:34:51 +0100

    lint: lint code

--- pink_lady.py
+++ pink_lady.py
@@ -1,13 +1,25 @@
+"""
+pink lady launch module
+"""
 import argparse
 
-from code.Reader import from_file
-from code.Writer import to_opk
+from code.reader import from_file
+from code.writer import to_opk
 
-parser = argparse.ArgumentParser(description='photogrammetric site conversion and manipulation software')
-parser.add_argument('-f', '--filepath',type=str, default= "", nargs=1, help='File path of the workfile')
-parser.add_argument('-skip', '--skip', type=int, default=None, nargs=1, help='Number of lines to be skipped before reading the file')
-parser.add_argument('-w', '--writer', type=str, choices=['opk'], help='Worksite output file format')
-parser.add_argument('-pr', '--pathreturn', type=str, default= "test/tmp/", nargs=1, help='Conversion path ex:"test/tmp/"')
+parser = argparse.ArgumentParser(description='photogrammetric site conversion'
+                                 + ' and manipulation software')
+parser.add_argument('-f', '--filepath',
+                    type=str, default="", nargs=1,
+                    help='File path of the workfile')
+parser.add_argument('-skip', '--skip',
+                    type=int, default=None, nargs=1,
+                    help='Number of lines to be skipped before reading the file')
+parser.add_argument('-w', '--writer',
+                    type=str, choices=['opk'],
+                    help='Worksite output file format')
+parser.add_argument('-pr', '--pathreturn',
+                    type=str, default="test/tmp/", nargs=1,
+                    help='Conversion path ex:"test/tmp/"')
 
 args = parser.parse_args()
 
@@ -21,5 +33,5 @@ else:
 print(args.writer)
 
 if args.writer == 'opk':
-    to_opk(args.pathreturn[0],work)
+    to_opk(args.pathreturn[0], work)
     print('file save well')

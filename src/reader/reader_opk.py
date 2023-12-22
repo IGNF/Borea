0ea0c0d75d00d2523c7f8b02842625c152f41a9e
ACornuIGN <antoine.cornu@ign.fr>
2023-12-22 10:37:42 +0100

    refactor: change structure file

--- src/reader/reader_opk.py
+++ src/reader/reader_opk.py
@@ -1,32 +1,11 @@
 """
-Photogrammetry site file reader module
+A script to read opk file
 """
 import numpy as np
 from src.worksite import Worksite
 
 
-def from_file(file: str, skip: int = None) -> Worksite:
-    """
-    Photogrammetric site file reading function
-
-    Args:
-        file (str): Path to the worksite.
-        skip (int): Number of lines to be skipped before reading the file, Default=None.
-
-    Returns:
-        Worksite: The worksite
-    """
-    ext = file.split(".")[-1]
-    if ext == "opk":
-        if skip is None:
-            skip = 1
-        work = from_opk(file, skip)
-    else:
-        print("Input file not taken into account")
-    return work
-
-
-def from_opk(file: str, skip: int = 1) -> Worksite:
+def read(file: str, skip: int) -> Worksite:
     """
     Reads an opk file to transform it into a Workside object
 
@@ -37,6 +16,9 @@ def from_opk(file: str, skip: int = 1) -> Worksite:
     Returns:
         Worksite: The worksite
     """
+    if skip == None:
+        skip = 1
+
     # Job name retrieval
     name_work = file.split('/')[-1]
     name_work = name_work.split('.')[0]

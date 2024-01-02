8d55ef3b8f0990c7b04b46d795fba629a1ebd1fe
ACornuIGN <antoine.cornu@ign.fr>
2024-01-02 16:18:04 +0100

    build: add function to read and store data to connecting point mes

--- src/datastruct/shot.py
+++ src/datastruct/shot.py
@@ -20,20 +20,21 @@ class Shot:
     pos_shot: np.array
     ori_shot: np.array
     name_cam: str
-    copoints: dict = field(default_factory = dict)
-    mat_R: np.array = field(init = False)
+    copoints: dict = field(default_factory=dict)
+    mat_rot: np.array = field(init=False)
 
     def __post_init__(self) -> np.array:
         """
         Build the rotation matrix with omega phi kappa
         """
-        Rx = np.array([[1,0,0],
-                       [0,np.cos(self.ori_shot[0]*np.pi/180),-np.sin(self.ori_shot[0]*np.pi/180)],
-                       [0,np.sin(self.ori_shot[0]*np.pi/180),np.cos(self.ori_shot[0]*np.pi/180)]])
-        Ry = np.array([[np.cos(self.ori_shot[1]*np.pi/180),0,np.sin(self.ori_shot[1]*np.pi/180)],
-                       [0,1,0],
-                       [-np.sin(self.ori_shot[1]*np.pi/180),0,np.cos(self.ori_shot[1]*np.pi/180)]])
-        Rz = np.array([[np.cos(self.ori_shot[2]*np.pi/180),-np.sin(self.ori_shot[2]*np.pi/180),0],
-                       [np.cos(self.ori_shot[2]*np.pi/180),np.cos(self.ori_shot[2]*np.pi/180),0],
-                       [0,0,1]])
-        self.mat_R = Rx @ Ry @ Rz
+        rx = np.array([[1, 0, 0],
+                       [0, np.cos(self.ori_shot[0]*np.pi/180), -np.sin(self.ori_shot[0]*np.pi/180)],
+                       [0, np.sin(self.ori_shot[0]*np.pi/180), np.cos(self.ori_shot[0]*np.pi/180)]])
+        ry = np.array([[np.cos(self.ori_shot[1]*np.pi/180), 0, np.sin(self.ori_shot[1]*np.pi/180)],
+                       [0, 1, 0],
+                       [-np.sin(self.ori_shot[1]*np.pi/180), 0,
+                        np.cos(self.ori_shot[1]*np.pi/180)]])
+        rz = np.array([[np.cos(self.ori_shot[2]*np.pi/180), -np.sin(self.ori_shot[2]*np.pi/180), 0],
+                       [np.cos(self.ori_shot[2]*np.pi/180), np.cos(self.ori_shot[2]*np.pi/180), 0],
+                       [0, 0, 1]])
+        self.mat_rot = rx @ ry @ rz
--- src/datastruct/worksite.py
+++ src/datastruct/worksite.py
@@ -1,6 +1,7 @@
 """
 Worksite data class module
 """
+import sys
 import numpy as np
 from src.datastruct.shot import Shot
 from src.datastruct.camera import Camera
@@ -20,6 +21,7 @@ class Worksite:
         self.name = name
         self.shots = {}
         self.cameras = {}
+        self.copoints = {}
 
     def add_shot(self, name_shot: str, pos_shot: np.array,
                  ori_shot: np.array, name_cam: str) -> None:
@@ -52,3 +54,35 @@ class Worksite:
                                            ppax=ppax,
                                            ppay=ppay,
                                            focal=focal)
+
+    def add_copoint(self, name_point: str, name_shot: str, x: float, y: float) -> None:
+        """
+        Add linking point between acquisition in two part
+        One in self.copoints a dict with name_point the key and list of acquisition the result
+        And One in self.shot[name_shot].copoints a dict whit
+        name_point the key and list of coordinate x (column) y (line) the result in list
+
+        Agrs:
+            name_point (str): Name of the connecting point
+            name_shot (str): Name of the acquisition
+            x (float): pixel position of the point in column
+            y (float): pixel position of the point in line
+        """
+        if name_shot not in self.shots:
+            print(f"The shot {name_shot} doesn't exist in list of shots")
+            sys.exit()
+
+        if name_point not in self.copoints:
+            self.copoints[name_point] = []
+
+        if name_point not in self.shots[name_shot].copoints:
+            self.shots[name_shot].copoints[name_point] = [x, y]
+        else:
+            print("\n :--------------------------:")
+            print("Warning : connecting point duplicate")
+            print(f"The point {name_point} already exists in the shot {name_shot}.")
+            print("Keep first point with coordinates " +
+                  f"{self.shots[name_shot].copoints[name_point]}")
+            print(":--------------------------:")
+
+        self.copoints[name_point].append(name_shot)

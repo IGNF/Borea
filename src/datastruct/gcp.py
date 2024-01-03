f59153df421c186cd60c653877720694dec06b54
ACornuIGN <antoine.cornu@ign.fr>
2024-01-03 16:54:23 +0100

    build: add new file for gcp

---
+++ src/datastruct/gcp.py
@@ -0,0 +1,22 @@
+"""
+Ground Control Point (GCP) class
+"""
+from dataclasses import dataclass
+import numpy as np
+
+
+@dataclass
+class GCP:
+    """
+    Ground Control Point definition
+
+    Args:
+        name_gcp (str): Name of the gcp.
+        code (int): ign code to differentiate between support points (1, 2, 3)
+                    and control points (11, 12, 13)
+                    1 means precision in Z, 2 in X and Y and 3 in X, Y, Z.
+        coor (numpy.array): Array of ground coordinate [X, Y, Z].
+    """
+    name_gcp: str
+    code: int
+    coor: np.array

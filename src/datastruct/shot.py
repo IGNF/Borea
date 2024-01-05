fix: corrects creation rotation matrix

src/datastruct/shot.py
@@ -36,6 +36,6 @@ class Shot:
                        [-np.sin(self.ori_shot[1]*np.pi/180), 0,
                         np.cos(self.ori_shot[1]*np.pi/180)]])
         rz = np.array([[np.cos(self.ori_shot[2]*np.pi/180), -np.sin(self.ori_shot[2]*np.pi/180), 0],
-                       [np.cos(self.ori_shot[2]*np.pi/180), np.cos(self.ori_shot[2]*np.pi/180), 0],
+                       [np.sin(self.ori_shot[2]*np.pi/180), np.cos(self.ori_shot[2]*np.pi/180), 0],
                        [0, 0, 1]])
         self.mat_rot = rx @ ry @ rz
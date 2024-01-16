3246b195108c5d8fbd06269a702cf2fc335d0a28
ACornuIGN <antoine.cornu@ign.fr>
2024-01-16 14:47:01 +0100

    test: change name test for understanding

--- test/test_euclideanproj.py
+++ test/test_euclideanproj.py
@@ -6,7 +6,7 @@ from src.geodesy.proj_engine import ProjEngine
 from src.geodesy.euclidean_proj import EuclideanProj
 
 
-def test_world_to_euclidean1():
+def test_world_to_euclidean_withfloat():
     proj = ProjEngine("EPSG:2154", {'geoc': 'EPSG:4964', 'geog': 'EPSG:7084', "geoid": ["fr_ign_RAF20"]})
     euproj = EuclideanProj(0,0,proj)
     x,y,z = euproj.world_to_euclidean(0,0,0)
@@ -15,7 +15,7 @@ def test_world_to_euclidean1():
     assert round(z) == 0
 
 
-def test_world_to_euclidean2():
+def test_world_to_euclidean_witharray3():
     proj = ProjEngine("EPSG:2154", {'geoc': 'EPSG:4964', 'geog': 'EPSG:7084', "geoid": ["fr_ign_RAF20"]})
     euproj = EuclideanProj(0,0,proj)
     x,y,z = euproj.world_to_euclidean(np.array([0,1,2]),np.array([0,1,2]),np.array([0,1,2]))
@@ -24,7 +24,7 @@ def test_world_to_euclidean2():
     assert round(z[0]) == 0
 
 
-def test_world_to_euclidean3():
+def test_world_to_euclidean_witharray1():
     proj = ProjEngine("EPSG:2154", {'geoc': 'EPSG:4964', 'geog': 'EPSG:7084', "geoid": ["fr_ign_RAF20"]})
     euproj = EuclideanProj(0,0,proj)
     x,y,z = euproj.world_to_euclidean(np.array([0]),np.array([0]),np.array([0]))
@@ -33,7 +33,7 @@ def test_world_to_euclidean3():
     assert round(z[0]) == 0
 
 
-def test_euclidean_to_world1():
+def test_euclidean_to_world1_withfloat():
     proj = ProjEngine("EPSG:2154", {'geoc': 'EPSG:4964', 'geog': 'EPSG:7084', "geoid": ["fr_ign_RAF20"]})
     euproj = EuclideanProj(0.0,0.0,proj)
     x,y,z = euproj.euclidean_to_world(0.0,0.0,0.0)
@@ -42,7 +42,7 @@ def test_euclidean_to_world1():
     assert round(z) == 0
 
 
-def test_euclidean_to_world2():
+def test_euclidean_to_world_witharray3():
     proj = ProjEngine("EPSG:2154", {'geoc': 'EPSG:4964', 'geog': 'EPSG:7084', "geoid": ["fr_ign_RAF20"]})
     euproj = EuclideanProj(0.0,0.0,proj)
     x,y,z = euproj.euclidean_to_world(np.array([0,1,2]),np.array([0,1,2]),np.array([0,1,2]))
@@ -51,7 +51,7 @@ def test_euclidean_to_world2():
     assert round(z[0]) == 0
 
 
-def test_euclidean_to_world3():
+def test_euclidean_to_world_witharray1():
     proj = ProjEngine("EPSG:2154", {'geoc': 'EPSG:4964', 'geog': 'EPSG:7084', "geoid": ["fr_ign_RAF20"]})
     euproj = EuclideanProj(0.0,0.0,proj)
     x,y,z = euproj.euclidean_to_world(np.array([0]),np.array([0]),np.array([0]))

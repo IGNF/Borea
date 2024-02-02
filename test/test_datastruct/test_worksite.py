4ea35d4c078c4d65a735fc2b38f112055507df65
ACornuIGN <antoine.cornu@ign.fr>
2024-02-02 16:10:08 +0100

    feat: add new function and more possibilities

--- test/test_datastruct/test_worksite.py
+++ test/test_datastruct/test_worksite.py
@@ -199,7 +199,7 @@ def test_calculate_world_to_image_gcp_testcodeNone():
     work.add_gcp('gcp_test', 13, np.array([815601.510, 6283629.280, 54.960]))
     work.add_gcp('gcp_test_test', 3, np.array([815601.510, 6283629.280, 54.960]))
     work.check_gcp = True
-    work.calculate_world_to_image_gcp(None)
+    work.calculate_world_to_image_gcp([])
     assert abs(work.shots['shot_test'].gcps['gcp_test'][0] - 24042.25) < 5
     assert abs(work.shots['shot_test'].gcps['gcp_test'][1] - 14781.17) < 5
     assert len(work.shots['shot_test'].gcps) == 2
@@ -217,7 +217,7 @@ def test_barycentre():
     assert bary[2] == 8
 
 
-def test_calculate_init_image_world_copoints_onecop_multiimg():
+def test_calculate_init_image_world_onecop_multiimg():
     work = Worksite("Test")
     work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test")
     work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test")
@@ -228,14 +228,14 @@ def test_calculate_init_image_world_copoints_onecop_multiimg():
     work.add_copoint('"1003"',"23FD1305x00026_01307",24120.2,10329.3)
     work.add_copoint('"1003"',"23FD1305x00026_01308",24161.49,5929.37)
     work.check_cop = True
-    work.calculate_init_image_world_copoints()
+    work.calculate_init_image_world()
     print(abs(work.cop_world['"1003"'][0] - 815601.510),abs(work.cop_world['"1003"'][1] - 6283629.280),abs(work.cop_world['"1003"'][2] - 54.960))
     assert abs(work.cop_world['"1003"'][0] - 815601.510) < 10000
     assert abs(work.cop_world['"1003"'][1] - 6283629.280) < 10000
     assert abs(work.cop_world['"1003"'][2] - 54.960) < 10000
 
 
-def test_calculate_init_image_world_copoints_onecopwithoneimg():
+def test_calculate_init_image_world_onecopwithoneimg():
     work = Worksite("Test")
     work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test")
     work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test")
@@ -246,10 +246,73 @@ def test_calculate_init_image_world_copoints_onecopwithoneimg():
     work.add_copoint('"1003"',"23FD1305x00026_01307",24120.2,10329.3)
     work.add_copoint('"1004"',"23FD1305x00026_01308",24161.49,5929.37)
     work.check_cop = True
-    work.calculate_init_image_world_copoints()
+    work.calculate_init_image_world()
     assert len(list(work.cop_world)) == 1
     assert '"1003"' in work.cop_world.keys()
     assert not('"1004"' in work.cop_world.keys())
+
+
+def test_calculate_init_image_world_withzeropoint():
+    work = Worksite("Test")
+    work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test")
+    work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test")
+    work.add_shot("23FD1305x00026_01308",np.array([814978.586,6283482.827,1771.799]),np.array([-0.181570631296, 0.001583051432,0.493526899473]),"cam_test")
+    work.set_proj("2154", "test/data/proj.json", "./test/data/")
+    work.add_camera('cam_test', 13210.00, 8502.00, 30975.00)
+    work.check_cop = False
+    work.calculate_init_image_world()
+    assert work.cop_world == {}
+
+
+def test_calculate_init_image_world_allgipoint():
+    work = Worksite("Test")
+    work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test")
+    work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test")
+    work.add_shot("23FD1305x00026_01308",np.array([814978.586,6283482.827,1771.799]),np.array([-0.181570631296, 0.001583051432,0.493526899473]),"cam_test")
+    work.add_shot("23FD1305x00054_05680",np.array([833124.675,6282303.066,1761.305]),np.array([-0.198514051868,-0.023898399551,0.190559923925]),"cam_test")
+    work.add_shot("23FD1305x00054_05681",np.array([833123.958,6282051.774,1761.056]),np.array([-0.222610811997,-0.045739865938,0.163818133681]),"cam_test")
+    work.set_proj("2154", "test/data/proj.json", "./test/data/")
+    work.add_camera('cam_test', 13210.00, 8502.00, 30975.00)
+    work.add_gipoint('"1003"',"23FD1305x00026_01306",24042.25,14781.17)
+    work.add_gipoint('"1003"',"23FD1305x00026_01307",24120.2,10329.3)
+    work.add_gipoint('"1003"',"23FD1305x00026_01308",24161.49,5929.37)
+    work.add_gipoint('"1005"',"23FD1305x00054_05680",22796.05,14371.27)
+    work.add_gipoint('"1005"',"23FD1305x00054_05681",22817.4,9930.73)
+    work.add_gcp('"1003"',13,np.array([815601.510,6283629.280,54.960]))
+    work.add_gcp('"1005"',3,np.array([833670.940,6281965.400,52.630]))
+    work.check_gip = True
+    work.calculate_init_image_world("gipoint")
+    print(abs(work.gip_world['"1003"'][0] - 815601.510),abs(work.gip_world['"1003"'][1] - 6283629.280),abs(work.gip_world['"1003"'][2] - 54.960))
+    print(abs(work.gip_world['"1005"'][0] - 833670.940),abs(work.gip_world['"1005"'][1] - 6281965.400),abs(work.gip_world['"1005"'][2] - 52.630))
+    assert len(list(work.gip_world)) == 2
+    assert '"1003"' in work.gip_world.keys()
+    assert '"1005"' in work.gip_world.keys()
+    assert abs(work.gip_world['"1003"'][0] - 815601.510) < 10000
+    assert abs(work.gip_world['"1003"'][1] - 6283629.280) < 10000
+    assert abs(work.gip_world['"1003"'][2] - 54.960) < 10000
+
+
+def test_calculate_init_image_world_gipoint13type():
+    work = Worksite("Test")
+    work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test")
+    work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test")
+    work.add_shot("23FD1305x00026_01308",np.array([814978.586,6283482.827,1771.799]),np.array([-0.181570631296, 0.001583051432,0.493526899473]),"cam_test")
+    work.add_shot("23FD1305x00054_05680",np.array([833124.675,6282303.066,1761.305]),np.array([-0.198514051868,-0.023898399551,0.190559923925]),"cam_test")
+    work.add_shot("23FD1305x00054_05681",np.array([833123.958,6282051.774,1761.056]),np.array([-0.222610811997,-0.045739865938,0.163818133681]),"cam_test")
+    work.set_proj("2154", "test/data/proj.json", "./test/data/")
+    work.add_camera('cam_test', 13210.00, 8502.00, 30975.00)
+    work.add_gipoint('"1003"',"23FD1305x00026_01306",24042.25,14781.17)
+    work.add_gipoint('"1003"',"23FD1305x00026_01307",24120.2,10329.3)
+    work.add_gipoint('"1003"',"23FD1305x00026_01308",24161.49,5929.37)
+    work.add_gipoint('"1005"',"23FD1305x00054_05680",22796.05,14371.27)
+    work.add_gipoint('"1005"',"23FD1305x00054_05681",22817.4,9930.73)
+    work.add_gcp('"1003"',13,np.array([815601.510,6283629.280,54.960]))
+    work.add_gcp('"1005"',3,np.array([833670.940,6281965.400,52.630]))
+    work.check_gip = True
+    work.calculate_init_image_world("gipoint", [13])
+    assert len(list(work.gip_world)) == 1
+    assert '"1003"' in work.gip_world.keys()
+    assert '"1005"' not in work.gip_world.keys()
 
 
 def test_eucli_intersection_2p():

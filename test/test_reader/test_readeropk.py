9077473bb3403a3312be263bffbe1449f08fa2ea
ACornuIGN <antoine.cornu@ign.fr>
2024-02-14 09:01:20 +0100

    test: add new test for opk reader

--- test/test_reader/test_readeropk.py
+++ test/test_reader/test_readeropk.py
@@ -60,3 +60,38 @@ def test_reader_opk_winpath():
         assert obj.shots["23FD1305x00062_07728"].ori_shot[2] == 0.453083308756
         assert obj.shots["23FD1305x00062_07728"].name_cam == "UCE-M3-f120-s06"
         assert len(obj.shots) == 7
+
+
+def test_reader_opk_bad_header_add_column():
+    work = Worksite("23FD1305_alt_test")
+    with pytest.raises(ValueError) as e_info:
+        obj = read_opk(INPUT_OPK_UBU, LINE, ['N','X','Y','Z','O','P','K','C','S'], UNIT_ANGLE, work)
+
+
+def test_reader_opk_bad_header_miss_column():
+    work = Worksite("23FD1305_alt_test")
+    with pytest.raises(ValueError) as e_info:
+        obj = read_opk(INPUT_OPK_UBU, LINE, ['N','X','Y','Z','O','P','K'], UNIT_ANGLE, work)
+
+
+def test_reader_opk_diffheader():
+    work = Worksite("23FD1305_alt_test")
+    obj = read_opk("./test/data/23FD1305_alt_NCPOKXYZ.OPK", LINE, ['S','N','C','P','O','K','X','Y','Z'], UNIT_ANGLE, work)
+    assert obj.name == "23FD1305_alt_test"
+    assert obj.shots["23FD1305x00026_01306"].name_shot == "23FD1305x00026_01306"
+    assert obj.shots["23FD1305x00026_01306"].pos_shot[0] == 814975.925
+    assert obj.shots["23FD1305x00026_01306"].pos_shot[1] == 6283986.148
+    assert obj.shots["23FD1305x00026_01306"].pos_shot[2] == 1771.280
+    assert obj.shots["23FD1305x00026_01306"].ori_shot[0] == -0.245070686036
+    assert obj.shots["23FD1305x00026_01306"].ori_shot[1] == -0.069409621323
+    assert obj.shots["23FD1305x00026_01306"].ori_shot[2] == 0.836320989726
+    assert obj.shots["23FD1305x00026_01306"].name_cam == "UCE-M3-f120-s06"
+    assert obj.shots["23FD1305x00062_07728"].name_shot == "23FD1305x00062_07728"
+    assert obj.shots["23FD1305x00062_07728"].pos_shot[0] == 838160.083
+    assert obj.shots["23FD1305x00062_07728"].pos_shot[1] == 6284788.913
+    assert obj.shots["23FD1305x00062_07728"].pos_shot[2] == 1766.066
+    assert obj.shots["23FD1305x00062_07728"].ori_shot[0] == -0.179920958782
+    assert obj.shots["23FD1305x00062_07728"].ori_shot[1] == 0.099806773228
+    assert obj.shots["23FD1305x00062_07728"].ori_shot[2] == 0.453083308756
+    assert obj.shots["23FD1305x00062_07728"].name_cam == "UCE-M3-f120-s06"
+    assert len(obj.shots) == 7

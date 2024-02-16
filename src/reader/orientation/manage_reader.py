ecda2c1160340510925e7b2380dce9c5a43ce2ed
ACornuIGN <antoine.cornu@ign.fr>
2024-02-16 15:14:45 +0100

    refactor: fix code with new header opk

--- src/reader/orientation/manage_reader.py
+++ src/reader/orientation/manage_reader.py
@@ -3,18 +3,15 @@ Photogrammetry site file reader module.
 """
 import importlib
 from src.datastruct.worksite import Worksite
-from src.utils.check_header import check_header_file
 
 
-def reader_orientation(file: str, line_take: list, header: list) -> Worksite:
+def reader_orientation(file: str, args: dict) -> Worksite:
     """
     Photogrammetric site file reading function.
 
     Args:
         file (str): Path to the worksite.
-        line_take (list): Interval of lines taken into account, [i, j] if i or j is None = :.
-                          e.g. [1, None] = [1:]
-        header (list): List of column type file.
+        args (dict): other information for reading the file
 
     Returns:
         Worksite: The worksite.
@@ -27,15 +24,11 @@ def reader_orientation(file: str, line_take: list, header: list) -> Worksite:
 
     name_work, ext = splitpath[-1].split(".")
 
-    header, type_z, type_angle = check_header_file(header)
-
     work = Worksite(name_work)
     try:
         my_module = importlib.import_module("src.reader.orientation.reader_" + ext.lower())
-        work = my_module.read(file, line_take, header, type_angle, work)
+        work = my_module.read(file, args, work)
     except ModuleNotFoundError as e:
         raise ModuleNotFoundError(f"{ext} file is not taken into account !!!") from e
 
-    work.type_z_shot = type_z
-    work.type_angle = type_angle
     return work

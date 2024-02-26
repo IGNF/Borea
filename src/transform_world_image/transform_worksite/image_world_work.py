c85cc92e17a07413f9b457e191a3b86f0bbf7c41
ACornuIGN <antoine.cornu@ign.fr>
2024-02-26 11:17:55 +0100

    refactor: change funciton calculate image world by intersection

--- src/transform_world_image/transform_worksite/image_world_work.py
+++ src/transform_world_image/transform_worksite/image_world_work.py
@@ -17,9 +17,57 @@ class ImageWorldWork(Workdata):
         name (str): Name of the worksite.
     """
 
-    # pylint: disable-next=too-many-locals too-many-branches
-    def calculate_init_image_world(self, type_point: str = "co_point",
-                                   control_type: list = None) -> None:
+    # pylint: disable-next=unused-argument
+    def copoints_position(self) -> None:
+        """
+        Calculates the field position of connection points using the least squares.
+
+        Args:
+            work (Worksite): The site on which you want to calculate the position of the copoints.
+        """
+        # pylint: disable-next=pointless-string-statement
+        """
+        #Initialization of world coordinate copoints
+        work.calculate_init_image_world_copoints()
+
+        for name_cop, coor_cop in work.cop_world.items():
+            bool_cond = True
+            it_count = 0
+            while bool_cond:
+                it_count +=1
+
+                coord_i, coord_j, data = [], [], []
+                v_res = np.zeros(2*len(work.copoints[name_cop]))
+                for name_shot in work.copoints[name_cop]:
+                    shot = work.shots[name_shot]
+                    cam = work.cameras[shot.name_cam]
+                    c_shot, l_shot = shot.world_to_image(coor_cop[0], coor_cop[1], coor_cop[2],
+                                                        cam, work.projeucli)
+                    coor_eucli = work.projeucli.world_to_euclidean(coor_cop[0],
+                                                                coor_cop[1],
+                                                                coor_cop[2])
+                    vect_a = coor_eucli - shot.pos_shot_eucli
+                    vect_u = shot.mat_rot_eucli @ vect_a
+
+                    mat_v = np.zeros((2 * len(vect_u[0]), 3))
+                    mat_v[::2, 0] = 1
+                    mat_v[::2, 2] = -vect_u[0] / vect_u[2]
+                    mat_v[1::2, 1] = 1
+                    mat_v[1::2, 2] = -vect_u[1] / vect_u[2]
+
+                    coord_i += [np.repeat(2 * pd_mes_pnt['index_mes'].to_numpy(), 6) +
+                                np.tile([0, 0, 0, 1, 1, 1], len(c_shot))]
+                    coord_j += [np.repeat(3 * pd_mes_pnt['index_pnt'].to_numpy(), 6) +
+                                np.tile([0, 1, 2, 0, 1, 2], len(c_shot))]
+                    data += [((np.tile(np.repeat(cam.focal / vect_u[2], 2), (3, 1)).T *
+                            mat_v @ shot.mat_rot_eucli).flatten())]
+                    v_res[2 * pd_mes_pnt['index_mes'].to_numpy()] =pd_mes_pnt["c"].to_numpy()-c_shot
+                    v_res[2 * pd_mes_pnt['index_mes'].to_numpy() + 1] = pd_mes_pnt["l"].to_numpy() -
+                    l_shot
+"""
+
+    def calculate_image_world_by_intersection(self, type_point: str = "co_point",
+                                              control_type: list = None) -> None:
         """
         Calculates the ground position of connecting point by intersection with
         the most distance between two shots or ground image point.
@@ -43,34 +91,50 @@ class ImageWorldWork(Workdata):
             check_gcp = True
 
         if check:
-            for name_p, item_p in points.items():  # Loop on points
-                if check_gcp and control_type != [] and self.gcps[name_p].code not in control_type:
+            for name_pt, list_shot in points.items():  # Loop on points
+                if check_gcp and control_type != [] and self.gcps[name_pt].code not in control_type:
                     continue
-                if len(item_p) == 1:
+                if len(list_shot) == 1:
                     continue
-                shot1 = ""
-                shot2 = ""
-                dist = 0
-                list_shot1 = item_p.copy()
-                list_shot2 = list_shot1.copy()
-                _ = list_shot1.pop(-1)
-                for name_shot1 in list_shot1:  # Double loop on shots where see the point
-                    _ = list_shot2.pop(0)
-                    for name_shot2 in list_shot2:
-                        pos_shot1 = self.shots[name_shot1].pos_shot
-                        pos_shot2 = self.shots[name_shot2].pos_shot
-                        new_dist = np.sqrt(np.sum((pos_shot1 - pos_shot2)**2))
-                        if new_dist > dist:
-                            dist = new_dist
-                            shot1 = name_shot1
-                            shot2 = name_shot2
-                coor = self.intersection_pt_in_2shot(name_p, self.shots[shot1], self.shots[shot2])
+
+                coor = self.comput_inter_in_2_more_distant_shot(name_pt, list_shot)
+
                 if type_point == "co_point":
-                    self.co_pts_world[name_p] = coor
+                    self.co_pts_world[name_pt] = coor
                 if type_point == "ground_img_pt":
-                    self.img_pts_world[name_p] = coor
+                    self.img_pts_world[name_pt] = coor
         else:
             print(f"There isn't {type_point} or bad spelling co_point / ground_img_pt.")
+
+    def comput_inter_in_2_more_distant_shot(self, name_pt: str, list_shot: list) -> np.ndarray:
+        """
+        Search for the two most distant images where the point is visible,
+        to calculate the point's position.
+
+        Args:
+            name_pt (str): Name of the point.
+            list_shot (list): List of name shot where the point is visible.
+        
+        Returns:
+            np.ndarray: World coordinate of the point.
+        """
+        shot1 = ""
+        shot2 = ""
+        dist = 0
+        list_shot1 = list_shot.copy()
+        list_shot2 = list_shot1.copy()
+        _ = list_shot1.pop(-1)
+        for name_shot1 in list_shot1:  # Double loop on shots where see the point
+            _ = list_shot2.pop(0)
+            for name_shot2 in list_shot2:
+                pos_shot1 = self.shots[name_shot1].pos_shot
+                pos_shot2 = self.shots[name_shot2].pos_shot
+                new_dist = np.sqrt(np.sum((pos_shot1 - pos_shot2)**2))
+                if new_dist > dist:
+                    dist = new_dist
+                    shot1 = name_shot1
+                    shot2 = name_shot2
+        return self.intersection_pt_in_2shot(name_pt, self.shots[shot1], self.shots[shot2])
 
     def intersection_pt_in_2shot(self, name_point: str, shot1: Shot, shot2: Shot) -> np.ndarray:
         """
@@ -141,52 +205,3 @@ class ImageWorldWork(Workdata):
         p1_eucli = point1 + ((b_v2*v1_v2 - b_v1*norme_v2)/(denum))*vect1
         p2_eucli = point2 + ((b_v2*norme_v1 - b_v1*v1_v2)/(denum))*vect2
         return (p1_eucli + p2_eucli) / 2
-
-    # pylint: disable-next=unused-argument
-    def copoints_position(self) -> None:
-        """
-        Calculates the field position of connection points using the least squares.
-
-        Args:
-            work (Worksite): The site on which you want to calculate the position of the copoints.
-        """
-        # pylint: disable-next=pointless-string-statement
-        """
-        #Initialization of world coordinate copoints
-        work.calculate_init_image_world_copoints()
-
-        for name_cop, coor_cop in work.cop_world.items():
-            bool_cond = True
-            it_count = 0
-            while bool_cond:
-                it_count +=1
-
-                coord_i, coord_j, data = [], [], []
-                v_res = np.zeros(2*len(work.copoints[name_cop]))
-                for name_shot in work.copoints[name_cop]:
-                    shot = work.shots[name_shot]
-                    cam = work.cameras[shot.name_cam]
-                    c_shot, l_shot = shot.world_to_image(coor_cop[0], coor_cop[1], coor_cop[2],
-                                                        cam, work.projeucli)
-                    coor_eucli = work.projeucli.world_to_euclidean(coor_cop[0],
-                                                                coor_cop[1],
-                                                                coor_cop[2])
-                    vect_a = coor_eucli - shot.pos_shot_eucli
-                    vect_u = shot.mat_rot_eucli @ vect_a
-
-                    mat_v = np.zeros((2 * len(vect_u[0]), 3))
-                    mat_v[::2, 0] = 1
-                    mat_v[::2, 2] = -vect_u[0] / vect_u[2]
-                    mat_v[1::2, 1] = 1
-                    mat_v[1::2, 2] = -vect_u[1] / vect_u[2]
-
-                    coord_i += [np.repeat(2 * pd_mes_pnt['index_mes'].to_numpy(), 6) +
-                                np.tile([0, 0, 0, 1, 1, 1], len(c_shot))]
-                    coord_j += [np.repeat(3 * pd_mes_pnt['index_pnt'].to_numpy(), 6) +
-                                np.tile([0, 1, 2, 0, 1, 2], len(c_shot))]
-                    data += [((np.tile(np.repeat(cam.focal / vect_u[2], 2), (3, 1)).T *
-                            mat_v @ shot.mat_rot_eucli).flatten())]
-                    v_res[2 * pd_mes_pnt['index_mes'].to_numpy()] =pd_mes_pnt["c"].to_numpy()-c_shot
-                    v_res[2 * pd_mes_pnt['index_mes'].to_numpy() + 1] = pd_mes_pnt["l"].to_numpy() -
-                    l_shot
-"""
--- test/test_transfo_world_image/test_imageworldwork.py
+++ test/test_transfo_world_image/test_imageworldwork.py
@@ -8,7 +8,7 @@ from src.worksite.worksite import Worksite
 PATH_DTM = "./dataset/MNT_France_25m_h_crop.tif"
 
 
-def test_calculate_init_image_world_onecop_multiimg():
+def test_calculate_image_world_by_intersection_onecop_multiimg():
     work = Worksite("Test")
     work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test","degree",True)
     work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test","degree",True)
@@ -22,14 +22,14 @@ def test_calculate_init_image_world_onecop_multiimg():
     work.type_z_shot = "altitude"
     work.type_z_data = "height"
     work.set_z_nadir_shot()
-    work.calculate_init_image_world()
+    work.calculate_image_world_by_intersection()
     print(abs(work.co_pts_world['"1003"'][0] - 815601.510),abs(work.co_pts_world['"1003"'][1] - 6283629.280),abs(work.co_pts_world['"1003"'][2] - 54.960))
     assert abs(work.co_pts_world['"1003"'][0] - 815601.510) < 1
     assert abs(work.co_pts_world['"1003"'][1] - 6283629.280) < 1
     assert abs(work.co_pts_world['"1003"'][2] - 54.960) < 1
 
 
-def test_calculate_init_image_world_onecopwithoneimg():
+def test_calculate_image_world_by_intersection_onecopwithoneimg():
     work = Worksite("Test")
     work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test","degree",True)
     work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test","degree",True)
@@ -43,25 +43,25 @@ def test_calculate_init_image_world_onecopwithoneimg():
     work.type_z_shot = "altitude"
     work.type_z_data = "height"
     work.set_z_nadir_shot()
-    work.calculate_init_image_world()
+    work.calculate_image_world_by_intersection()
     assert len(list(work.co_pts_world)) == 1
     assert '"1003"' in work.co_pts_world.keys()
     assert not('"1004"' in work.co_pts_world.keys())
 
 
-def test_calculate_init_image_world_withzeropoint():
+def test_calculate_image_world_by_intersection_withzeropoint():
     work = Worksite("Test")
     work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test","degree",True)
     work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test","degree",True)
     work.add_shot("23FD1305x00026_01308",np.array([814978.586,6283482.827,1771.799]),np.array([-0.181570631296, 0.001583051432,0.493526899473]),"cam_test","degree",True)
     work.set_proj(2154, "dataset/proj.json", "./dataset/")
     work.add_camera('cam_test', 13210.00, 8502.00, 30975.00, 26460.00, 17004.00)
-    work.calculate_init_image_world()
+    work.calculate_image_world_by_intersection()
     print("The print is normal")
     assert work.co_pts_world == {}
 
 
-def test_calculate_init_image_world_allgipoint():
+def test_calculate_image_world_by_intersection_allgipoint():
     work = Worksite("Test")
     work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test","degree",True)
     work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test","degree",True)
@@ -81,7 +81,7 @@ def test_calculate_init_image_world_allgipoint():
     work.type_z_shot = "altitude"
     work.type_z_data = "height"
     work.set_z_nadir_shot()
-    work.calculate_init_image_world("ground_img_pt")
+    work.calculate_image_world_by_intersection("ground_img_pt")
     print(abs(work.img_pts_world['"1003"'][0] - 815601.510),abs(work.img_pts_world['"1003"'][1] - 6283629.280),abs(work.img_pts_world['"1003"'][2] - 54.960))
     print(abs(work.img_pts_world['"1005"'][0] - 833670.940),abs(work.img_pts_world['"1005"'][1] - 6281965.400),abs(work.img_pts_world['"1005"'][2] - 52.630))
     assert len(list(work.img_pts_world)) == 2
@@ -92,7 +92,7 @@ def test_calculate_init_image_world_allgipoint():
     assert abs(work.img_pts_world['"1003"'][2] - 54.960) < 1
 
 
-def test_calculate_init_image_world_gipoint13type():
+def test_calculate_image_world_by_intersection_gipoint13type():
     work = Worksite("Test")
     work.add_shot("23FD1305x00026_01306",np.array([814975.925,6283986.148,1771.280]),np.array([-0.245070686036,-0.069409621323,0.836320989726]),"cam_test","degree",True)
     work.add_shot("23FD1305x00026_01307",np.array([814977.593,6283733.183,1771.519]),np.array([-0.190175545509,-0.023695590794,0.565111690487]),"cam_test","degree",True)
@@ -112,7 +112,7 @@ def test_calculate_init_image_world_gipoint13type():
     work.type_z_shot = "altitude"
     work.type_z_data = "height"
     work.set_z_nadir_shot()
-    work.calculate_init_image_world("ground_img_pt", [13])
+    work.calculate_image_world_by_intersection("ground_img_pt", [13])
     assert len(list(work.img_pts_world)) == 1
     assert '"1003"' in work.img_pts_world.keys()
     assert '"1005"' not in work.img_pts_world.keys()

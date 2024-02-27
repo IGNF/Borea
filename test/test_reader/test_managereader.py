230f1ce7fb88810c6cef3fa52c41cb2fe8f60d52
ACornuIGN <antoine.cornu@ign.fr>
2024-02-27 10:41:01 +0100

    test: add test for new funciton

--- test/test_reader/test_managereader.py
+++ test/test_reader/test_managereader.py
@@ -5,13 +5,10 @@ from src.reader.orientation.manage_reader import reader_orientation
 
 INPUT_OPK_UBU = "./dataset/23FD1305_alt_test.OPK"
 INPUT_OPK_WIN = ".\\dataset\\23FD1305_alt_test.OPK"
-ARGS = {"interval": [2, None],
-        "header": "N X Y Z O P K C",
-        "unit_angle": "degree",
-        "linear_alteration":True}
+
 
 def test_reader_file_ubupath():
-    obj = reader_orientation(INPUT_OPK_UBU, ARGS)
+    obj = reader_orientation(INPUT_OPK_UBU, {"interval": [2, None],"header": "N X Y Z O P K C","unit_angle": "degree","linear_alteration":True})
     assert obj.name == "23FD1305_alt_test"
     assert obj.shots["23FD1305x00026_01306"].name_shot == "23FD1305x00026_01306"
     assert obj.shots["23FD1305x00026_01306"].pos_shot[0] == 814975.925
@@ -33,7 +30,7 @@ def test_reader_file_ubupath():
 
 
 def test_reader_file_winpath():
-    obj = reader_orientation(INPUT_OPK_WIN, ARGS)
+    obj = reader_orientation(INPUT_OPK_WIN, {"interval": [2, None],"header": "N X Y Z O P K C","unit_angle": "degree","linear_alteration":True})
     assert obj.name == "23FD1305_alt_test"
     assert obj.shots["23FD1305x00026_01306"].name_shot == "23FD1305x00026_01306"
     assert obj.shots["23FD1305x00026_01306"].pos_shot[0] == 814975.925

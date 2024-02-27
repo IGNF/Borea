4eac5da8b9659ceb871a5c5f7266aca754a8e3ea
ACornuIGN <antoine.cornu@ign.fr>
2024-02-27 08:51:33 +0100

    refactor: add funciton manage image world and refactor intersect and direct

--- src/transform_world_image/transform_worksite/image_world_work.py
+++ src/transform_world_image/transform_worksite/image_world_work.py
@@ -1,5 +1,5 @@
 """
-Image world transformation module for Worksite
+Image world transformation module for worksite
 """
 import numpy as np
 from src.datastruct.workdata import Workdata
@@ -17,65 +17,21 @@ class ImageWorldWork(Workdata):
         name (str): Name of the worksite.
     """
 
-    # pylint: disable-next=unused-argument
-    def copoints_position(self) -> None:
+    def manage_image_world(self, type_point: str = "co_point", type_process: str = "intersection",
+                           control_type: list = None) -> None:
         """
-        Calculates the field position of connection points using the least squares.
-
-        Args:
-            work (Worksite): The site on which you want to calculate the position of the copoints.
-        """
-        # pylint: disable-next=pointless-string-statement
-        """
-        #Initialization of world coordinate copoints
-        work.calculate_init_image_world_copoints()
-
-        for name_cop, coor_cop in work.cop_world.items():
-            bool_cond = True
-            it_count = 0
-            while bool_cond:
-                it_count +=1
-
-                coord_i, coord_j, data = [], [], []
-                v_res = np.zeros(2*len(work.copoints[name_cop]))
-                for name_shot in work.copoints[name_cop]:
-                    shot = work.shots[name_shot]
-                    cam = work.cameras[shot.name_cam]
-                    c_shot, l_shot = shot.world_to_image(coor_cop[0], coor_cop[1], coor_cop[2],
-                                                        cam, work.projeucli)
-                    coor_eucli = work.projeucli.world_to_euclidean(coor_cop[0],
-                                                                coor_cop[1],
-                                                                coor_cop[2])
-                    vect_a = coor_eucli - shot.pos_shot_eucli
-                    vect_u = shot.mat_rot_eucli @ vect_a
-
-                    mat_v = np.zeros((2 * len(vect_u[0]), 3))
-                    mat_v[::2, 0] = 1
-                    mat_v[::2, 2] = -vect_u[0] / vect_u[2]
-                    mat_v[1::2, 1] = 1
-                    mat_v[1::2, 2] = -vect_u[1] / vect_u[2]
-
-                    coord_i += [np.repeat(2 * pd_mes_pnt['index_mes'].to_numpy(), 6) +
-                                np.tile([0, 0, 0, 1, 1, 1], len(c_shot))]
-                    coord_j += [np.repeat(3 * pd_mes_pnt['index_pnt'].to_numpy(), 6) +
-                                np.tile([0, 1, 2, 0, 1, 2], len(c_shot))]
-                    data += [((np.tile(np.repeat(cam.focal / vect_u[2], 2), (3, 1)).T *
-                            mat_v @ shot.mat_rot_eucli).flatten())]
-                    v_res[2 * pd_mes_pnt['index_mes'].to_numpy()] =pd_mes_pnt["c"].to_numpy()-c_shot
-                    v_res[2 * pd_mes_pnt['index_mes'].to_numpy() + 1] = pd_mes_pnt["l"].to_numpy() -
-                    l_shot
-"""
-
-    def calculate_image_world_by_intersection(self, type_point: str = "co_point",
-                                              control_type: list = None) -> None:
-        """
-        Calculates the ground position of connecting point by intersection with
-        the most distance between two shots or ground image point.
+        Process to calcule image coordinate to world coordinate
 
         Args:
             type_point (str): "co_point" or "ground_img_pt" depending on what you want to calculate.
+            type_process (str): Type of process you want to use.
+                                * "intersection" by intersect bundle of point in each shot
+                                * "direct" by mean the result of image_to_world_shot()  in each shot
             control_type (list): type controle for gcp.
         """
+        if type_process not in ["intersection", "direct"]:
+            raise ValueError(f"type_process {type_process} is incorrect, ['intersection','direct']")
+
         if control_type is None:
             control_type = []
 
@@ -83,28 +39,76 @@ class ImageWorldWork(Workdata):
         if type_point == "co_point":
             points = self.co_points
             check = bool(points)
-            check_gcp = False
 
         if type_point == "ground_img_pt":
             points = self.ground_img_pts
             check = bool(points)
-            check_gcp = True
 
         if check:
-            for name_pt, list_shot in points.items():  # Loop on points
-                if check_gcp and control_type != [] and self.gcps[name_pt].code not in control_type:
-                    continue
-                if len(list_shot) == 1:
-                    continue
+            if type_process == "intersection":
+                self.calculate_image_world_by_intersection(points, control_type, type_point)
 
-                coor = self.comput_inter_in_2_more_distant_shot(name_pt, list_shot)
-
+            if type_process == "direct":
                 if type_point == "co_point":
-                    self.co_pts_world[name_pt] = coor
-                if type_point == "ground_img_pt":
-                    self.img_pts_world[name_pt] = coor
+                    world_pt = self.co_pts_world
+                else:
+                    world_pt = self.img_pts_world
+                self.compute_mean_image_world(points, world_pt, type_point, control_type)
+
         else:
             print(f"There isn't {type_point} or bad spelling co_point / ground_img_pt.")
+
+    def compute_mean_image_world(self, points: dict, world_pt: dict, type_point: str,
+                                 control_type: list) -> None:
+        """
+        Calculates the mean of the result x y z of the point for each shot where it's visible.
+
+        Args:
+            points (dict): Dictionary of points tu calcules position by intersection.
+            type_point (str): "co_point" or "ground_img_pt" depending on what you want to calculate.
+            control_type (list): List of code gcp to take.
+                                 To take all points or co_point, control_type = [].
+        """
+        for name_pt, list_shot in points.items():
+            if control_type != [] and self.gcps[name_pt].code not in control_type:
+                continue
+            coor = []
+            for name_shot in list_shot:
+                shot = self.shots[name_shot]
+                cam = self.cameras[shot.name_cam]
+                if type_point == "co_point":
+                    coor_pt_img = shot.co_points[name_pt]
+                else:
+                    coor_pt_img = shot.ground_img_pts[name_pt]
+
+                coor.append(ImageWorldShot(shot, cam).image_to_world(coor_pt_img,
+                                                                     self.type_z_data,
+                                                                     self.type_z_shot))
+            world_pt[name_pt] = np.mean(coor, axis=0)
+
+    def calculate_image_world_by_intersection(self, points: dict, control_type:
+                                              list, type_point: str) -> None:
+        """
+        Calculates the ground position of connecting point by intersection with
+        the most distance between two shots or ground image point.
+
+        Args:
+            points (dict): Dictionary of points tu calcules position by intersection.
+            control_type (list): Type controle for gcp.
+            type_point (str): "co_point" or "ground_img_pt" depending on what you want to calculate.
+        """
+        for name_pt, list_shot in points.items():  # Loop on points
+            if control_type != [] and self.gcps[name_pt].code not in control_type:
+                continue
+            if len(list_shot) == 1:
+                continue
+
+            coor = self.comput_inter_in_2_more_distant_shot(name_pt, list_shot)
+
+            if type_point == "co_point":
+                self.co_pts_world[name_pt] = coor
+            if type_point == "ground_img_pt":
+                self.img_pts_world[name_pt] = coor
 
     def comput_inter_in_2_more_distant_shot(self, name_pt: str, list_shot: list) -> np.ndarray:
         """
@@ -114,7 +118,7 @@ class ImageWorldWork(Workdata):
         Args:
             name_pt (str): Name of the point.
             list_shot (list): List of name shot where the point is visible.
-        
+
         Returns:
             np.ndarray: World coordinate of the point.
         """

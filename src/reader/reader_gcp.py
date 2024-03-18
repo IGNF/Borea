55d4b9339b48b1803b7403f7a85895006df041af
ACornuIGN <antoine.cornu@ign.fr>
2024-03-18 11:54:37 +0100

    fix: read file point with one file

--- src/reader/reader_gcp.py
+++ src/reader/reader_gcp.py
@@ -6,24 +6,23 @@ import numpy as np
 from src.worksite.worksite import Worksite
 
 
-def read_gcp(files: list, work: Worksite) -> None:
+def read_gcp(file: str, work: Worksite) -> None:
     """
-    Read all files gcp.
+    Read file gcp.
 
     Agrs:
-        files (list): Path list of files gcp.
+        file (str): Path of file gcp.
         work (Worksite): Worksite which needs gcp.
     """
-    for file in files:
-        try:
-            with open(Path(PureWindowsPath(file)), 'r', encoding="utf-8") as file_gcp:
-                for gcp in file_gcp.readlines():
-                    if gcp != '\n':
-                        name_gcp, code_gcp, x, y, z = gcp.split()
-                        work.add_gcp(name_gcp, int(code_gcp), np.array([float(x),
-                                                                        float(y),
-                                                                        float(z)]))
-                file_gcp.close()
-        except FileNotFoundError as e:
-            raise FileNotFoundError(f"The path {file} is incorrect !!!") from e
+    try:
+        with open(Path(PureWindowsPath(file)), 'r', encoding="utf-8") as file_gcp:
+            for gcp in file_gcp.readlines():
+                if gcp != '\n':
+                    name_gcp, code_gcp, x, y, z = gcp.split()
+                    work.add_gcp(name_gcp, int(code_gcp), np.array([float(x),
+                                                                    float(y),
+                                                                    float(z)]))
+            file_gcp.close()
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"The path {file} is incorrect !!!") from e
     work.check_gcp = True

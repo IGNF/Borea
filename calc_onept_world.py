adf1756c2f4c559b03693df572fca93acbe46253
ACornuIGN <antoine.cornu@ign.fr>
2024-04-03 14:50:17 +0200

    refactor: change location parser and cut args control

--- calc_onept_world.py
+++ calc_onept_world.py
@@ -2,8 +2,8 @@
 Main to calculate world coodinate of the image point.
 """
 import argparse
-from src.parser.parser_func.args_add_shot import args_add_shot, process_add_shot
-from src.parser.parser_func.args_pt3d import args_add_pt3d, process_world_image
+from src.parser.parser_adata.args_add_shot import args_add_shot, process_add_shot
+from src.parser.parser_adata.args_pt3d import args_add_pt3d, process_world_image
 
 
 parser = argparse.ArgumentParser(description='Calculate world coodinate of the image point.')
--- opk_control.py
+++ opk_control.py
@@ -3,7 +3,7 @@ Main to control opk file
 """
 import argparse
 from src.parser.parser_opk.args_read_opk import args_reading_opk, process_args_read_opk
-from src.parser.parser_adata.args_control import args_control, process_args_control
+from src.parser.parser_func.args_control import args_control, process_args_control
 
 
 parser = argparse.ArgumentParser(description='Photogrammetric site control opk file')

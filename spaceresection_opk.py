99847b7a731e0ed749aef89d16a817cecf721d4a
ACornuIGN <antoine.cornu@ign.fr>
2024-04-09 16:27:00 +0200

    feat: add new main function to use space resection on point

---
+++ spaceresection_opk.py
@@ -0,0 +1,18 @@
+"""
+Main to calculate 6 externals parameters of shots and save in opk file.
+"""
+import argparse
+
+from src.parser.parser_func.args_spaceresection import args_space_resection, process_space_resection
+from src.parser.parser_opk.args_write_opk import args_writing_opk, process_args_write_opk
+
+
+parser = argparse.ArgumentParser(description='Space resection of point file image and world'
+                                             ' to calculate 6 externals parameters of shots')
+parser = args_space_resection(parser)
+parser = args_writing_opk(parser)
+
+args = parser.parse_args()
+
+work = process_space_resection(args)
+process_args_write_opk(args, work)

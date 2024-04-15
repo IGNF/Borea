32239c708425d3fd1ca3871453499dba144c3707
ACornuIGN <antoine.cornu@ign.fr>
2024-04-15 10:20:07 +0200

    test: fix bug existing file in gitaction

--- test/test_format/test_conical.py
+++ test/test_format/test_conical.py
@@ -10,6 +10,10 @@ from src.datastruct.shot import Shot
 from src.datastruct.camera import Camera
 
 
+def setup_module(module): # run before the first test
+    os.makedirs("./test/tmp", exist_ok=True)
+
+
 def test_save_conical():
     shot = Shot("test_shot", np.array([814975.925,6283986.148,1771.280]), np.array([-0.245070686036,-0.069409621323,0.836320989726]), "test_cam", 'degree',True, "opk")
     cam = Camera("test_cam", 13210.00, 8502.00, 30975.00, 26460, 17004, 4e-6)

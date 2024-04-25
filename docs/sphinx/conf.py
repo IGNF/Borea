ee186cd80169351698659e1181cd9c294083eed2
ACornuIGN <antoine.cornu@ign.fr>
2024-04-25 11:02:57 +0200

    docs: update sphinx docs

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../implements"))
+sys.path.insert(0, os.path.abspath("../.."))
 
 project = 'Pink_Lady'
 copyright = '2023, Antoine Cornu et Nicolas Laurain'

c285ca51ac10352ccd550500db9e6c30b2b84dc8
ACornuIGN <antoine.cornu@ign.fr>
2024-06-20 15:37:15 +0200

    refactor : del size pixel in camera file

--- borea/datastruct/camera.py
+++ borea/datastruct/camera.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass, field
 @dataclass
 class Camera:
     """
-    Shot class definition.
+    Camera class definition.
 
     Args:
         name_camera (str): Name of the camera.
@@ -16,7 +16,6 @@ class Camera:
         focal (float): Focal of the camera.
         width (int): Width of the image in pixel.
         height (int): Height of the image in pixel.
-        pixel_size (float): Size of pixel in meter (mandatory if you save in conical file).
     """
     name_camera: str
     ppax: float = field(default=None)
@@ -24,4 +23,3 @@ class Camera:
     focal: float = field(default=None)
     width: int = field(default=None)
     height: int = field(default=None)
-    pixel_size: float = field(default=None)
